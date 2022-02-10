664ea43ca76cbe6d7309375c47df8d104a632746
slivoviy <s01110011@yandex.ru>
2022-02-10 22:36:58 +0300

    Properties for Enemy.cs

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour {
@@ -9,12 +7,24 @@ public class Enemy : MonoBehaviour {
     // они скалируются (200 - 400)
     private int lowerHealthBound = 40;
     private int higherHealthBound = 80;
-    
+
     private float health;
     /// <summary>
     /// Здоровье дедлайна. Снижается при клике и пассивно от улучшений
     /// </summary>
-    public float Health { get => health; set { health = value; } }
+    public float Health { get => health; set => health = value; }
+
+    private int timeToDo;
+    /// <summary>
+    /// Время, выдаваемое на то, чтобы сделать таску
+    /// </summary>
+    public int TimeToDo { get => timeToDo; set => timeToDo = value; }
+    
+    private string taskName;
+    /// <summary>
+    /// Название таска
+    /// </summary>
+    public string TaskName { get => taskName; set => taskName = value; }
     
     private void Awake() {
         Init();
