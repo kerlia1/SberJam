dc120b795c900ef05c1e9e85077ba57290c58036
slivoviy <s01110011@yandex.ru>
2022-02-10 18:23:48 +0300

    Добавил урон по клику на кнопку, написал частично архитектуру для Enemy

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,17 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class Enemy : MonoBehaviour {
+
+    //Диапазон здоровья врага, нужно посчитать, сейчас значения взяты от балды
+    //Здоровье должно быть кратно 5, поэтому значения маленькие, при инициализации
+    // они скалируются (200 - 400)
+    private int lowerHealthBound = 40;
+    private int higherHealthBound = 80;
+    
+    private float health;
+    /// <summary>
+    /// Здоровье дедлайна. Снижается при клике и пассивно от улучшений
+    /// </summary>
+    public float Health { get => health; set { health = value; } }
+    
+    private void Awake() {
+        Init();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+
+    /// <summary>
+    /// Инициализация параметров при спавне врага
+    /// </summary>
+    public void Init() {
+        Health = Random.Range(lowerHealthBound, higherHealthBound) * 5;
     }
 }
--- Assets/Scripts/GameplayController.cs
+++ Assets/Scripts/GameplayController.cs
@@ -2,17 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameplayController : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class GameplayController : MonoBehaviour {
+    //Объекты, через которые проходит взаимодействие
+    GameObject player;
+    GameObject enemy;
+    
+    //Переменные связанные с классами
+    private float playerDamagePerClick;
+
+    private void Awake() {
+        Init();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+
+    /// <summary>
+    /// Инициализация всех параметров.
+    /// </summary>
+    public void Init() {
+        player = GameObject.Find("Player");
+        enemy = GameObject.Find("Enemy");
+
+        playerDamagePerClick = player.GetComponent<Player>().DamagePerClick;
+    }
+
+    /// <summary>
+    /// Наносит урон при клике
+    /// </summary>
+    public void DamageEnemyOnClick() {
+        enemy.GetComponent<Enemy>().Health -= playerDamagePerClick;
+        Debug.Log(enemy.GetComponent<Enemy>().Health);
     }
 }
